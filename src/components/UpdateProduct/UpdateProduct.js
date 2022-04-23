c6cc5da050b05ee367438e6885f067c57da6931a
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-04-23 18:16:59 +0600

    update product feature implemented

--- src/components/UpdateProduct/UpdateProduct.js
+++ src/components/UpdateProduct/UpdateProduct.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate, useParams } from "react-router-dom";
 
 const UpdateProduct = () => {
     const navigate = useNavigate();
     const { id } = useParams();
-    console.log(id);
+    const [product, setProduct] = useState({});
+    useEffect(() => {
+        fetch(`http://localhost:5000/product/${id}`)
+            .then((res) => res.json())
+            .then((data) => setProduct(data));
+    }, [id]);
     const { register, handleSubmit } = useForm();
+    const [successMessage, setSuccessMessage] = useState("");
     const onSubmit = (data) => {
         console.log(data);
+        fetch(`http://localhost:5000/product/${id}`, {
+            method: "PUT",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.modifiedCount > 0) {
+                    setSuccessMessage("Your Product is updated successfully");
+                }
+            });
     };
     return (
         <div>
@@ -22,10 +41,19 @@ const UpdateProduct = () => {
                 <h2 className="text-center text-primary my-2">
                     Update your product
                 </h2>
+                <h4 className="text-center text-success">{successMessage}</h4>
                 <form
                     className="my-3 w-50 mx-auto p-3 shadow rounded"
                     onSubmit={handleSubmit(onSubmit)}
                 >
+                    {product && (
+                        <div>
+                            <h4>Previous Product: </h4>
+                            <p>
+                                Name: {product.name} Price: ${product.price}
+                            </p>
+                        </div>
+                    )}
                     <input
                         className="form-control mb-2"
                         placeholder="Name"
@@ -46,7 +74,7 @@ const UpdateProduct = () => {
                     <input
                         className="btn btn-primary px-4 py-2 w-50"
                         type="submit"
-                        value="Add Product"
+                        value="Update"
                     />
                 </form>
             </div>
